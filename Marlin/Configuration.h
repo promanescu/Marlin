e97204c2391bd43d6fc59196a9b713768a29e686
promanescu <75334270+promanescu@users.noreply.github.com>
2022-06-19 00:42:30 +0200

    Successful Flash

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -98,7 +98,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_BTT_SKR_MINI_E3_V2_0
 #endif
 
 /**
@@ -1074,7 +1074,7 @@
 #define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 // Force the use of the probe for Z-axis homing
-//#define USE_PROBE_FOR_Z_HOMING
+#define USE_PROBE_FOR_Z_HOMING
 
 /**
  * Z_MIN_PROBE_PIN
--- Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_V2_0.h
+++ Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_V2_0.h
@@ -52,11 +52,11 @@
   #define PS_ON_PIN                        PC13  // Power Supply Control
 #endif
 
-#define FAN1_PIN                           PC7
+//#define FAN1_PIN                           PC7
 
-#ifndef CONTROLLER_FAN_PIN
-  #define CONTROLLER_FAN_PIN               FAN1_PIN
-#endif
+//#ifndef CONTROLLER_FAN_PIN
+//  #define CONTROLLER_FAN_PIN               FAN1_PIN
+//#endif
 
 #if HAS_TMC_UART
   /**
